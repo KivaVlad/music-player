c1ae09b791ccbab6533a6ffa07c3395abbee9735
Vladislav <melnikovv717@gmail.com>
2023-12-16 21:54:03 +0300

    update player

--- js/index.js
+++ js/index.js
@@ -1,26 +1,21 @@
 const player = document.querySelector('audio');
-const playButton = document.getElementById('play');
-const pauseButton = document.getElementById('pause');
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
-const songAuthor = document.querySelector('.audio_author');
-const songName = document.querySelector('.audio_name');
-const progress = document.querySelector('.progress__bar');
-const currentSongTime = document.querySelector('.player__current-time');
-const totalSongTime = document.querySelector('.player__total-time');
+const songAuthor = document.getElementById('song_author');
+const songName = document.getElementById('song_name');
+const progress = document.getElementById('progress_bar');
+const currentSongTime = document.getElementById('song_time_counter');
+const totalSongTime = document.getElementById('song_total-time');
 const randomButton = document.getElementById('random');
 const randomActiveButton = document.getElementById('random-active');
 const repeatButton = document.getElementById('repeat');
 const repeatActiveButton = document.getElementById('repeat-active');
-const playlistContainer = document.querySelector('.list');
-const openPlaylistButton = document.getElementById('open-playlist');
-const closePlaylistButton = document.getElementById('close-playlist');
-const volumeOnButton = document.getElementById('volume-on');
-const volumeOffButton = document.getElementById('volume-off');
-const volumeControl = document.querySelector('.volume_control_container');
-const volumeProgress = document.querySelector('.volume_input');
-const volumeValue = document.querySelector('.volume_value');
+const volumeProgress = document.getElementById('player-volume');
+const volumeValueContainer = document.querySelector('.player-song__volume_value');
+const volumeValue = document.getElementById('volume-value');
+const playlistContainer = document.querySelector('.playlist');
 
+let isPlaying = false;
 let isRandom = false;
 let isRepeat = false;
 let currentSongIndex = 0;
@@ -38,260 +33,262 @@ const playlist = [
     {author: 'Linkin Park', name: 'X-Ecutioner Style', full_name: 'Linkin Park - X-Ecutioner Style'},
 ]
 
-// Загружаем выбранную песню
-function loadSong(song) {
-    songAuthor.innerHTML = song.author;
-    songName.innerHTML = song.name;
-    player.src = `./audio/${song.full_name}.mp3`;
-}
+const wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#0079FF',
+    progressColor: '#697C9A',
+    hideScrollbar: true,
+    barWidth: 3,
+    height: 70,
+    media: player,
+})
 
-loadSong(playlist[currentSongIndex]);
 
-// Play
-function playAudio() {
-    player.play();
-    playButton.classList.add('hidden');
-    pauseButton.classList.remove('hidden');
-}
+function init() {
+    // Загружаем выбранную песню
+    function loadSong(song) {
+        songAuthor.innerHTML = song.author;
+        songName.innerHTML = song.name;
+        wavesurfer.load(`./audio/${song.full_name}.mp3`);
 
-// Pause
-function pauseAudio() {
-    player.pause();
-    playButton.classList.remove('hidden')
-    pauseButton.classList.add('hidden');
-}
-
-// Следующая песня
-function nextAudio() {
-    if (isRepeat === true) {
-        currentSongIndex = currentSongIndex;  
-    } else {
-        if (isRandom === true) {
-            currentSongIndex = Math.ceil(Math.random() * playlist.length);
-        } else {
-            currentSongIndex++
-        }
-    }
-    
-    if (currentSongIndex === playlist.length) {
-        currentSongIndex = 0;
-    }
-
-    loadSong(playlist[currentSongIndex]);
-    pauseAudio();
-    playAudio();
-}
-
-// Предыдущая песня
-function prevAudio() {
-    if (currentSongIndex === 0) { 
-        currentSongIndex = playlist.length;
-    }
-
-    if (isRepeat === true) {
-        currentSongIndex = currentSongIndex;  
-    } else {
-        if (isRandom === true) {
-            currentSongIndex = Math.ceil(Math.random() * playlist.length);
-        } else {
-            currentSongIndex--
+        if (isPlaying === true) {
+            wavesurfer.on('ready', () => wavesurfer.play());
         }
     }
 
     loadSong(playlist[currentSongIndex]);
-    pauseAudio();
-    playAudio();
-}
 
-// Progress bar
-function updateProgress(e) {
-    const {duration, currentTime} = e.srcElement;
-    const prograssPercent = Math.ceil((currentTime / duration) * 100);
-
-    if (prograssPercent > 0) { 
-        progress.value = prograssPercent;
-    }
-
-    currentTime > 0 ? currentSongTime.innerHTML = formattedSeconds(currentTime) : currentSongTime.innerHTML = '00:00';
-    currentTime > 0 ? totalSongTime.innerHTML = formattedSeconds(duration) : totalSongTime.innerHTML = '00:00';
-}
-
-// Перемотка при клике в progress bar
-function setProgress(e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const duration = player.duration;
-    player.currentTime = (clickX / width) * duration;
-}
-
-// Переводим время 
-function formattedSeconds(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.floor(seconds % 60);
-    const formattedTime = `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
-    return formattedTime;
-}
-
-// Случайное воспроизведение
-function randomAudio() {
-    isRandom = true;
-    isRepeat = false;
-    iconsClasses();
-}
-
-// Отменить случайное воспроизведение
-function cancelRandomAudio() {
-    isRandom = false;
-    iconsClasses();
-}
-
-// Повтор одного трека
-function repeatAudio() {
-    isRepeat = true;
-    isRandom = false;
-    iconsClasses()
-}
-
-// Отменить повтор трека
-function cancelRepeatAudio() {
-    isRepeat = false;
-    iconsClasses()
-}
-
-// Меняем классы иконок
-function iconsClasses() {
-    if (isRandom === true) {
-        randomButton.classList.add('hidden');
-        randomActiveButton.classList.remove('hidden');
-        repeatButton.classList.remove('hidden');
-        repeatActiveButton.classList.add('hidden');
-    } else {
-        randomButton.classList.remove('hidden');
-        randomActiveButton.classList.add('hidden');
-    }
-
-    if (isRepeat === true) {
-        repeatButton.classList.add('hidden');
-        repeatActiveButton.classList.remove('hidden');
-        randomButton.classList.remove('hidden');
-        randomActiveButton.classList.add('hidden');
-    } else {
-        repeatButton.classList.remove('hidden');
-        repeatActiveButton.classList.add('hidden');
-    }
-}
-
-// Открыть плейлист
-function openPlaylist() {
-    const listContainer = document.getElementById('playlist');
-    listContainer.classList.remove('playlist');
-    listContainer.classList.add('playlist_active');
-}
-
-// Закрыть плейлист
-function closePlaylist() {
-    const listContainer = document.getElementById('playlist');
-    listContainer.classList.remove('playlist_active');
-    listContainer.classList.add('playlist');
-}
-
-// Отрисовываем плейлист
-function initPlaylist() {
+    // Отрисовываем плейлист
     for (let i = 0; i < playlist.length; i++) {
         playlistContainer.innerHTML += 
         `
-            <div class="playlist_item">
-                <h4 class="song_title">${playlist[i].full_name}</h4>
-                <div class="playlist_item__icon_wrapper">
-                    <img src="./assets/icons/white-play.png" data-id="${i}" id="play-icon" alt="" />
-                    <img src="./assets/icons/write-pause.png" id="pause-icon" alt="" />
+            <div class="playlist-audio__container">
+                <div class="playlist-audio__name-wrapper">
+                    <div class="player-nav__play-pause__container">
+                        <img src="./assets/icons/play.png" alt="play" id="play" data-id="${i}" />
+                        <img src="./assets/icons/pause.png" alt="pause" class="hidden" id="pause" />
+                    </div>
+                    <h4 class="playlist-audio__song_title">${playlist[i].full_name}</h4>
                 </div>
             </div>
         `
     }
+
     
-    const playIcons = document.querySelectorAll('#play-icon');
-    const pauseIcons = document.querySelectorAll('#pause-icon');
+    // Кнопки play & pause
+    const playButtons = document.querySelectorAll('#play');
+    const pauseButtons = document.querySelectorAll('#pause');
 
-    // При клике на play получаем id выбранного элемента
-    playIcons.forEach((icon) => icon.addEventListener('click', (e) => {
-        playIcons.forEach((icon) => icon.classList.remove('hidden'));
-        icon.classList.add('hidden');
-        pauseIcons.forEach((icon) => icon.classList.remove('hidden'));
 
-        const { id } = e.target.dataset;
-        currentSongIndex = id;
+    // Следующая песня
+    function nextAudio() {
+        if (isRepeat === true) {
+            currentSongIndex = currentSongIndex;  
+        } else {
+            if (isRandom === true) {
+                currentSongIndex = Math.ceil(Math.random() * playlist.length);
+            } else {
+                currentSongIndex++
+            }
+        }
+        
+        if (currentSongIndex === playlist.length) {
+            currentSongIndex = 0;
+        }
+
+        playButtons.forEach((btn) => {
+            btn.classList.remove('hidden');
+            
+            const id = btn.dataset.id;
+            if (currentSongIndex === +id) {
+                btn.classList.add('hidden');
+                pauseButtons.forEach((btn) => btn.classList.remove('hidden'));
+            }
+
+            if (btn.dataset?.set) btn.classList.add('hidden');
+            
+        })
+
+        isPlaying = true;
+        player.play();
         loadSong(playlist[currentSongIndex]);
-        playAudio();
-    }))
-
-    // При клике на pause
-    pauseIcons.forEach((icon) => icon.addEventListener('click', () => {
-        pauseIcons.forEach((icon) => icon.classList.remove('hidden'));
-        icon.classList.add('hidden');
-        playIcons.forEach((icon) => icon.classList.remove('hidden'));
-
-        pauseAudio();
-    }))
-}
-
-initPlaylist();
-
-// Отключить звук
-function volumeOff() {
-    volumeOnButton.classList.add('hidden');
-    volumeOffButton.classList.remove('hidden');
-
-    volumeProgress.value = 0;
-    volumeProgress.min = 0;
-    player.volume = 0;
-    volumeValue.innerHTML = `0%`;
-}
-
-// Включить звук
-function volumeOn() {
-    volumeOnButton.classList.remove('hidden');
-    volumeOffButton.classList.add('hidden');
-}
-
-// Показать контейнер с регулировкой громкости
-function visibleVolume() {
-    volumeControl.style.display = 'flex';
-}
-
-// Скрыть контейнер с регулировкой громкости
-function hiddenVolume() {
-    volumeControl.style.display = 'none';
-}
-
-// Настройка звука
-function setVolume() {
-    const value = volumeProgress.value;
-    player.volume = value / 100;
-    volumeValue.innerHTML = `${value}%`;
-
-    if (+value === 0) {
-        volumeOff();
-    } else {
-        volumeOn();
     }
+
+
+    // Предыдущая песня
+    function prevAudio() {
+        if (currentSongIndex === 0) { 
+            currentSongIndex = playlist.length;
+        }
+
+        if (isRepeat === true) {
+            currentSongIndex = currentSongIndex;  
+        } else {
+            if (isRandom === true) {
+                currentSongIndex = Math.ceil(Math.random() * playlist.length);
+            } else {
+                currentSongIndex--
+            }
+        }
+
+        playButtons.forEach((btn) => {
+            btn.classList.remove('hidden');
+
+            const id = btn.dataset.id;
+            if (currentSongIndex === +id) {
+                btn.classList.add('hidden');
+                pauseButtons.forEach((btn) => btn.classList.remove('hidden'));
+            }
+
+            if (btn.dataset?.set) btn.classList.add('hidden');
+        })
+
+        isPlaying = true;
+        player.play();
+        loadSong(playlist[currentSongIndex]);
+    }
+
+
+    // Progress bar
+    function updateProgress(e) {
+        const {duration, currentTime} = e.srcElement;
+        const prograssPercent = Math.ceil((currentTime / duration) * 100);
+
+        if (prograssPercent > 0) { 
+            progress.value = prograssPercent;
+        }
+
+        currentTime > 0 ? currentSongTime.innerHTML = formattedSeconds(currentTime) : currentSongTime.innerHTML = '00:00';
+        currentTime > 0 ? totalSongTime.innerHTML = formattedSeconds(duration) : totalSongTime.innerHTML = '00:00';
+    }
+
+
+    // Форматируем время 
+    function formattedSeconds(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = Math.floor(seconds % 60);
+        const formattedTime = `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+        return formattedTime;
+    }
+
+
+    // Перемотка при клике в progress bar
+    function setProgress(e) {
+        const width = this.clientWidth;
+        const clickX = e.offsetX;
+        const duration = player.duration;
+        player.currentTime = (clickX / width) * duration;
+    }
+
+
+    // Настройка звука
+    function setVolume() {
+        const value = volumeProgress.value;
+        player.volume = value / 100;
+        volumeValue.innerHTML = `${value}%`;
+    }
+
+
+    // Случайное воспроизведение
+    function randomAudio() {
+        isRandom = true;
+        isRepeat = false;
+        iconsClasses();
+    }
+
+    // Отменить случайное воспроизведение
+    function cancelRandomAudio() {
+        isRandom = false;
+        iconsClasses();
+    }
+
+    // Повтор одного трека
+    function repeatAudio() {
+        isRepeat = true;
+        isRandom = false;
+        iconsClasses()
+    }
+
+    // Отменить повтор трека
+    function cancelRepeatAudio() {
+        isRepeat = false;
+        iconsClasses()
+    }
+
+    // Меняем классы иконок
+    function iconsClasses() {
+        if (isRandom === true) {
+            randomButton.classList.add('hidden');
+            randomActiveButton.classList.remove('hidden');
+            repeatButton.classList.remove('hidden');
+            repeatActiveButton.classList.add('hidden');
+        } else {
+            randomButton.classList.remove('hidden');
+            randomActiveButton.classList.add('hidden');
+        }
+
+        if (isRepeat === true) {
+            repeatButton.classList.add('hidden');
+            repeatActiveButton.classList.remove('hidden');
+            randomButton.classList.remove('hidden');
+            randomActiveButton.classList.add('hidden');
+        } else {
+            repeatButton.classList.remove('hidden');
+            repeatActiveButton.classList.add('hidden');
+        }
+    }
+
+
+    // при клике на play
+    playButtons.forEach((btn) => btn.addEventListener('click', (e) => {
+        const { id } = e.target.dataset;
+
+        if (id) {
+            currentSongIndex = +id;
+            loadSong(playlist[currentSongIndex]);
+        }
+
+        playButtons.forEach((btn) => btn.classList.remove('hidden'));
+        btn.classList.add('hidden');
+        playButtons.forEach((btn) => {
+            if (btn.dataset?.set) btn.classList.add('hidden');
+
+            const id = btn.dataset.id;
+            if (currentSongIndex === +id) btn.classList.add('hidden');
+
+        });
+        pauseButtons.forEach((btn) => btn.classList.remove('hidden'));
+
+        isPlaying = true;
+        player.play();
+    }))
+
+
+    // при клике на pause
+    pauseButtons.forEach((btn) => btn.addEventListener('click', () => {
+        pauseButtons.forEach((btn) => btn.classList.remove('hidden'));
+        btn.classList.add('hidden');
+        playButtons.forEach((btn) => btn.classList.remove('hidden'));
+
+        isPlaying = false;
+        player.pause();
+    }))
+
+
+    // EventListeners
+    prevButton.addEventListener('click', prevAudio);
+    nextButton.addEventListener('click', nextAudio);
+    player.addEventListener('ended', nextAudio);
+    player.addEventListener('timeupdate', updateProgress);
+    progress.addEventListener('click', setProgress);
+    randomButton.addEventListener('click', randomAudio);
+    randomActiveButton.addEventListener('click', cancelRandomAudio);
+    repeatButton.addEventListener('click', repeatAudio);
+    repeatActiveButton.addEventListener('click', cancelRepeatAudio);
+    volumeProgress.addEventListener('click', setVolume);
+    volumeProgress.addEventListener('mousemove', () => volumeValueContainer.style.display = 'flex');
+    volumeProgress.addEventListener('mouseout', () => volumeValueContainer.style.display = 'none');
+    volumeValue.innerHTML = `${volumeProgress.value}%`;
 }
 
-playButton.addEventListener('click', playAudio);
-pauseButton.addEventListener('click', pauseAudio);
-nextButton.addEventListener('click', nextAudio);
-prevButton.addEventListener('click', prevAudio);
-player.addEventListener('timeupdate', updateProgress);
-player.addEventListener('ended', nextAudio);
-progress.addEventListener('click', setProgress);
-randomButton.addEventListener('click', randomAudio);
-randomActiveButton.addEventListener('click', cancelRandomAudio);
-repeatButton.addEventListener('click', repeatAudio);
-repeatActiveButton.addEventListener('click', cancelRepeatAudio);
-openPlaylistButton.addEventListener('click', openPlaylist);
-closePlaylistButton.addEventListener('click', closePlaylist);
-volumeOnButton.addEventListener('click', volumeOff);
-volumeOffButton.addEventListener('click', volumeOn);
-document.querySelector('.volume_icons_container').addEventListener('mouseover', visibleVolume);
-document.querySelector('.volume_icons_container').addEventListener('mouseout', hiddenVolume);
-volumeProgress.addEventListener('click', setVolume);
+init();
